e44f69eb99af57132026a30d3dcf69fc3c49638b
Carl Pulley <carlp@cakesolutions.net>
2014-12-16 17:16:39 +0000

    Hard coded debug logging (for now!)

--- core/src/main/scala/scala/spores/package.scala
+++ core/src/main/scala/scala/spores/package.scala
@@ -50,7 +50,7 @@ package object spores {
   }
 
   // TOGGLE DEBUGGING
-  private val isDebugEnabled = System.getProperty("spores.debug", "false").toBoolean
+  private val isDebugEnabled = true //System.getProperty("spores.debug", "false").toBoolean
   private[spores] def debug(s: => String): Unit =
     if (isDebugEnabled) println(s"*** $s ***")
 
